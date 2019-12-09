940f24e6e03fdeaf9892fb69de5efd43e0819a01
Renican <inkomirikan@gmail.com>
2019-12-09 20:52:28 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FanoCompression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
